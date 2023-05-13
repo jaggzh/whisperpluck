49649da2e540b2763f3d45ac721d42f4be7a0edb
Jaggzh <jaggz.h@gmail.com>
2023-05-13 14:31:12 -0700

    switched to arecord, and cleaning up settings, and adding server use instead of running whisper directly

---
+++ Makefile
@@ -0,0 +1,12 @@
+all:
+	@echo "Nothing here really. Use 'make vi' to edit the project with vim"
+
+vi:
+	vim \
+		README.md \
+		Makefile \
+		settings.sh \
+		whisper-auto \
+		whisper-kill-rec \
+		server-flask/app.py
+		
---
+++ server-flask/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from flask import Flask, abort, request
+import whisper
+from tempfile import NamedTemporaryFile
+
+# Load the Whisper model:
+model = whisper.load_model('base')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def handler():
+    if not request.files:
+        # If the user didn't submit any files, return a 400 (Bad Request) error.
+        abort(400)
+
+    # For each file, let's store the results in a list of dictionaries.
+    results = []
+
+    # Loop over every file that the user submitted.
+    for filename, handle in request.files.items():
+        # Create a temporary file.
+        # The location of the temporary file is available in `temp.name`.
+        temp = NamedTemporaryFile()
+        # Write the user's uploaded file to the temporary file.
+        # The file will get deleted when it drops out of scope.
+        handle.save(temp)
+        # Let's get the transcript of the temporary file.
+        result = model.transcribe(temp.name)
+        print(f"Transcribed -> {result['text']}")
+        # Now we can store the result object for this file.
+        results.append({
+            'filename': filename,
+            'transcript': result['text'],
+        })
+
+    # This will be automatically converted to JSON.
+    return {'results': results}
